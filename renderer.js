616cada28b330018e007514febf922e4bee60819
NahdaaJ <nahdaajawed@gmail.com>
2025-09-03 17:21:55 +0100

    Add task complete toggle

--- renderer.js
+++ renderer.js
@@ -3,6 +3,14 @@ const tasksContainer = document.getElementById("task-container");
 const checkIconSource = "images/flower.png";
 const deleteIconSource = "images/close.png";
 
+
+// document.querySelectorAll(".check-icon").forEach(function(icon) {
+//     icon.addEventListener("click", function(event) {
+//         const taskDiv = event.target.parentElement;
+//         completeTaskToggle(taskDiv);
+//     });
+// });
+
 function addTask () {
     if (inputEntry.value !== "") {        
         const taskTitle = inputEntry.value.trim();
@@ -14,6 +22,10 @@ function addTask () {
         const checkIcon = document.createElement("img");
         checkIcon.src = checkIconSource;
         checkIcon.className = "check-icon";
+        checkIcon.addEventListener("click", function(event) {
+            const taskDiv = event.target.parentElement;
+            completeTaskToggle(taskDiv);
+        });
 
         const taskText = document.createElement("p");
         taskText.textContent = taskTitle;
@@ -29,5 +41,14 @@ function addTask () {
         
         tasksContainer.appendChild(newTask);
         inputEntry.value = "";
+    }
+}
+
+function completeTaskToggle(task) {
+    const isCompleted = task.getAttribute("completed") === "true";
+    if (isCompleted) {
+        task.setAttribute("completed", "false");
+    } else {
+        task.setAttribute("completed", "true");
     }
 }
